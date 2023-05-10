9ae0587b1a0c3b6c695a47f11dadd4e436b9850f
Kaewmanee Marasri <64160234@go.buu.ac.th>
2023-05-10 15:55:20 +0700

    💚 create nav group

---
+++ src/components/GroupList.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <div class="list">
+      <ul>
+        <li>
+          <div class="avatar">
+            <div class="w-11 rounded-full">
+              <img src="https://scontent.fbkk2-5.fna.fbcdn.net/v/t39.30808-6/327006796_686653046481563_74172586481304136_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGEAvjUZgjr4Ldk4_bDMapkG2uM1p0Y8UUba4zWnRjxRdd_nyCjhj_TqXpQpgmaW-RYNMlcF3dmK1ThbCdpxbYG&_nc_ohc=QGvM-bTnDWYAX-qh3yz&_nc_ht=scontent.fbkk2-5.fna&oh=00_AfAYrhB3-GkHYQ9YjZn3qjfqiftV0wBCZ6wpsw4SUhfI1Q&oe=64613760" />
+            </div>
+          </div>
+        </li>
+        <li>
+            <div class="avatar">
+            <div class="w-11 rounded-full">
+              <img src="https://scontent.fbkk2-5.fna.fbcdn.net/v/t39.30808-6/327006796_686653046481563_74172586481304136_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGEAvjUZgjr4Ldk4_bDMapkG2uM1p0Y8UUba4zWnRjxRdd_nyCjhj_TqXpQpgmaW-RYNMlcF3dmK1ThbCdpxbYG&_nc_ohc=QGvM-bTnDWYAX-qh3yz&_nc_ht=scontent.fbkk2-5.fna&oh=00_AfAYrhB3-GkHYQ9YjZn3qjfqiftV0wBCZ6wpsw4SUhfI1Q&oe=64613760" />
+            </div>
+          </div>
+        </li>
+        <li>
+            <div class="avatar">
+            <div class="w-11 rounded-full">
+              <img src="https://scontent.fbkk2-5.fna.fbcdn.net/v/t39.30808-6/327006796_686653046481563_74172586481304136_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGEAvjUZgjr4Ldk4_bDMapkG2uM1p0Y8UUba4zWnRjxRdd_nyCjhj_TqXpQpgmaW-RYNMlcF3dmK1ThbCdpxbYG&_nc_ohc=QGvM-bTnDWYAX-qh3yz&_nc_ht=scontent.fbkk2-5.fna&oh=00_AfAYrhB3-GkHYQ9YjZn3qjfqiftV0wBCZ6wpsw4SUhfI1Q&oe=64613760" />
+            </div>
+          </div>
+        </li>
+        <li>
+            <div class="avatar">
+            <div class="w-11 rounded-full">
+              <img src="https://scontent.fbkk2-5.fna.fbcdn.net/v/t39.30808-6/327006796_686653046481563_74172586481304136_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGEAvjUZgjr4Ldk4_bDMapkG2uM1p0Y8UUba4zWnRjxRdd_nyCjhj_TqXpQpgmaW-RYNMlcF3dmK1ThbCdpxbYG&_nc_ohc=QGvM-bTnDWYAX-qh3yz&_nc_ht=scontent.fbkk2-5.fna&oh=00_AfAYrhB3-GkHYQ9YjZn3qjfqiftV0wBCZ6wpsw4SUhfI1Q&oe=64613760" />
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+<style scoped>
+.list {
+  background-color: #212A3E;
+  border-radius: 0 5px 5px 0;
+  height: 100vh;
+  width: 5vw;
+  min-width: 50px;
+  position: fixed;
+}
+ul{
+    text-align: center;
+    margin-top: 20px;
+
+}
+li{
+    margin-bottom: 10px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,13 @@
 <script lang="ts" setup>
+import GroupList from "@/components/GroupList.vue";
 import NavigationBarVue from "@/components/NavigationBarVue.vue";
 </script>
 <template>
   <div>
     <NavigationBarVue></NavigationBarVue>
-    <div class="home"></div>
+    <div class="home">
+      <GroupList></GroupList>
+    </div>
   </div>
 </template>
 <style scoped>
