74b0965fc4e697390700c7b1d16ee4da6c642930
Kaewmanee Marasri <64160234@go.buu.ac.th>
2023-05-14 21:12:00 +0700

    ⚡️ create service calender

--- src/services/calender.service.ts
+++ src/services/calender.service.ts
@@ -1,5 +1,29 @@
+import { Calendar } from "@fullcalendar/core";
 import http from "./axios";
 
-// const createCalender =()=>{
-//     return http.post()
-// }
+const createCalender = (calender: Calendar) => {
+  return http.post(`/calenders`, calender);
+};
+const updateCalender = (id: number, calender: Calendar) => {
+  return http.patch(`/calenders/${id}`, calender);
+};
+
+const deleteCalender = (id: number) => {
+  return http.delete(`/calenders/${id}`);
+};
+
+const joinCalender = (code: string, calender: Calendar) => {
+  return http.patch(`/calenders/join/${code}`, calender);
+};
+
+const deleteMember = (id: number, calender: Calendar) => {
+  return http.patch(`/calenders/delete-member/${id}`, calender);
+};
+
+export default {
+  createCalender,
+  updateCalender,
+  deleteCalender,
+  joinCalender,
+  deleteMember,
+};
