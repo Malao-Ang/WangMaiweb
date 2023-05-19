d5f1f0e5d1957846c3402c088311230bdd861725
Kaewmanee Marasri <64160234@go.buu.ac.th>
2023-05-19 19:38:06 +0700

    fix page login

--- src/views/LoginVue.vue
+++ src/views/LoginVue.vue
@@ -1,22 +1,45 @@
 <script lang="ts" setup>
-import { useUserStore } from '@/store/user.store';
+import { useUserStore } from "@/store/user.store";
 
 const userStore = useUserStore();
-
 </script>
 <template>
   <div>
     <div class="login">
-      <div class="card w-96 glass ">
-        
-        <button class="btn gap-2 btn-outline btn-primary btn-wid" @click="userStore.login">
-          <img
-            src="https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-09-512.png"
-            class="h-6 w-6"
-          />
+      <div class="hero min-h-screen bg-base-200">
+        <div class="hero-content flex-col lg:flex-row-reverse">
+          <div class="text-center lg:text-left ">
+            <img
+        :src="require(`@/assets/images/37.jpg`)"
+        class="img-hero rounded-lg shadow-2xl"
+      />
+            <h1 class="text-5xl font-bold">Login To WANGMAI</h1>
+            
+            <p class="py-6">
+           Welcome to WANGMAI(ว่างมั้ย).
+            </p>
+          </div>
+          <div
+            class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100"
+          >
+            <div class="card-body">
+            
+                  <span class="label-text mb-10 text-center text-xl text-bold">Login With Google</span>
+                  <button
+                    class="btn gap-2 btn-outline btn-primary btn-wid"
+                    @click="userStore.login"
+                  >
+                    <img
+                      src="https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-09-512.png"
+                      class="h-6 w-6"
+                    />
 
-          Sign in with Google
-        </button>
+                    Sign in with Google
+                  </button>
+               
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -30,13 +53,18 @@ const userStore = useUserStore();
   justify-content: center;
 }
 .box-login {
-
   height: 65vh;
   padding: 20px 20px;
   margin-left: 15px;
-  margin-right: 15px; display: flex;
+  margin-right: 15px;
+  display: flex;
   align-items: center;
   justify-content: center;
 }
-
+.img-hero{
+  width: 30vw;
+  min-width: 230px;
+  margin-bottom: 20px;
+  margin: 0 auto;
+}
 </style>
