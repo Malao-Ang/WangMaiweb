8f114e2e8549740e409d0cc4e9ef0699836341e9
Kaewmanee Marasri <64160234@go.buu.ac.th>
2023-05-20 17:46:21 +0700

    fix port

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 // import router from "@/router";
 
 const instance = axios.create({
-  baseURL: process.env.PORT || "http://localhost:3000/api",
+  baseURL: process.env.PORT 
 });
 
 function delay(time: number) {
